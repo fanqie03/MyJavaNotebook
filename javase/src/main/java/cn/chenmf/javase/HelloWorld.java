3cf6e651e7d68ecbe7b5392c3771404368df4efd
mfchen <1242733702@qq.com>
2018-12-29 14:59:18 +0800

    add javaee and javase module

---
+++ javase/src/main/java/cn/chenmf/javase/HelloWorld.java
@@ -0,0 +1,13 @@
+package cn.chenmf.javase;
+
+/**
+ * @author         skiner
+ * @Date           2018/12/29
+ * @Description
+ * @Modified by
+ */
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("HelloWorld");
+    }
+}
